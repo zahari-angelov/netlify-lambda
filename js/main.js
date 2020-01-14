637fdad111be47ac0b748885a3d3524c7f44180c
Zahari Angelov <prolimitbg@gmail.com>
2020-01-14 10:33:58 +0200

    another commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const fetchUsers = async () =>
-    await (await fetch('http://localhost:9000/getusers')).json();
+    await (await fetch('/.netlify/functions/getusers')).json();
 
 fetchUsers().then(data => {
     userList = document.querySelector('#users');
